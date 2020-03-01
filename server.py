adb0021deae7e779894a05b1e3ce0cc644319149
IncredibLink <link@langchao.org>
2020-02-29 22:36:23 -0500

    Introduce annotation ranking system

--- server.py
+++ server.py
@@ -6,6 +6,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins='*', async_handlers=True)
 
+def initialize_annotation(annotation):
+  if 'rank' not in annotation:
+    annotation.update(rank=1)
+  return annotation
+
+def write_back():
+  for n_question in range(1, n_questions + 1):
+    d = [article for article in article_data if article['category'] == 'Question {}'.format(n_question)]
+    with open('./data/q{}_train.json'.format(n_question), 'w') as f:
+      f.write(json.dumps(d))
+
 article_data = []
 n_questions = 1
 for n_question in range(1, n_questions + 1):
@@ -14,18 +25,14 @@ for n_question in range(1, n_questions + 1):
     for j in json_data:
       if j['answer'] == 1:
         j.update(category='Question {}'.format(n_question))
-        if ('annotations' not in j):
+        if 'annotations' not in j:
           j.update(annotations=[])
+        j['annotations'] = [initialize_annotation(annotation) for annotation in j['annotations']]
         article_data.append(j)
+write_back()
 
 def get_room_name(category, article_id):
   return str(category) + ' ' + str(article_id)
-
-def write_back():
-  for n_question in range(1, n_questions + 1):
-    d = [article for article in article_data if article['category'] == 'Question {}'.format(n_question)]
-    with open('./data/q{}_train.json'.format(n_question), 'w') as f:
-      f.write(json.dumps(d))
 
 @app.route('/ping')
 def ping():
@@ -68,15 +75,17 @@ def on_add_annotation(data):
   article_id = data['articleId']
   annotator = data['annotator']
   sentence_index = data['sentenceIndex']
+  rank = data['rank']
   ann = {
     'articleId': article_id,
     'sentenceIndex': sentence_index,
     'annotator': annotator,
-    'category': category
+    'category': category,
+    'rank': rank
   }
 
   for i in range(0, len(article_data)):
-    if article_data[i]['id'] == article_id:
+    if article_data[i]['id'] == article_id and article_data[i]['category'] == category:
       if (len(article_data[i]['sentences']) < sentence_index):
         return
       for annotation in article_data[i]['annotations']:
@@ -103,7 +112,7 @@ def on_remove_annotation(data):
   }
 
   for i in range(0, len(article_data)):
-    if article_data[i]['id'] == article_id:
+    if article_data[i]['id'] == article_id and article_data[i]['category'] == category:
       if (len(article_data[i]['sentences']) < sentence_index):
         return
       for annotation in article_data[i]['annotations']:
@@ -111,6 +120,37 @@ def on_remove_annotation(data):
           article_data[i]['annotations'].remove(annotation)
           room = join_room(get_room_name(category, article_id))
           emit('remove annotation', ann, room=room, broadcast=True)
+          write_back()
+          return
+
+@socketio.on('update annotation rank')
+def on_update_annotation_rank(data):
+  category = data['category']
+  article_id = data['articleId']
+  annotator = data['annotator']
+  sentence_index = data['sentenceIndex']
+  rank = data['rank']
+
+  ann = {
+    'articleId': article_id,
+    'sentenceIndex': sentence_index,
+    'annotator': annotator,
+    'category': category,
+    'rank': rank
+  }
+
+  for i in range(0, len(article_data)):
+    if article_data[i]['id'] == article_id and article_data[i]['category'] == category:
+      if (len(article_data[i]['sentences']) < sentence_index):
+        return
+      for j in range(0, len(article_data[i]['annotations'])):
+        annotation = article_data[i]['annotations'][j]
+        print(annotation)
+        if annotation['sentenceIndex'] == sentence_index and annotation['annotator'] == annotator:
+          print('found')
+          article_data[i]['annotations'][j]['rank'] = rank
+          room = join_room(get_room_name(category, article_id))
+          emit('update annotation rank', ann, room=room, broadcast=True)
           write_back()
           return
 
